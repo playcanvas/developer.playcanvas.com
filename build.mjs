playcanvas/developer.playcanvas.com

commit 797201a65bfedb2299cb1daff07f71a113893347

Update `@metalsmith/permalinks` to 2.5.1 (#522)
* Update @metalsmith/permalinks

        "@metalsmith/permalinks": "2.5.1",
        "node-fetch": "^3.3.2",

* Update build.mjs

Adjust plugin order.  Add error checking.

See https://github.com/playcanvas/developer.playcanvas.com/issues/521

* Update head.hbs

Use permalink property which replaces path.

see: https://github.com/playcanvas/developer.playcanvas.com/issues/521

* Update build.mjs

Remove commented code.

build.mjs
@@ -1,5 +1,5 @@
-import fs from 'fs';
-import path from 'path';
+import fs from 'node:fs';
+import path from 'node:path';
 import { fileURLToPath } from 'url';
 import handlebars from 'handlebars';
 import { marked } from 'marked';
@@ -61,6 +61,11 @@ handlebars.registerHelper('lang-selector-close', (lang) => {
 // Convert relativeURL with a locale like en/manual to a full url with the
 // desired locale e.g. https://developer.playcanvas.com/ja/user-manual
 handlebars.registerHelper('locale-url', (locale, relativeUrl) => {
+    if (!relativeUrl) {
+        console.warn(`build.mjs : undefined relativeUrl passed to helper.`);
+        return '';
+    }
+
     relativeUrl = path.join(locale, relativeUrl.substring(2));
     const url = new URL(relativeUrl, 'https://developer.playcanvas.com/');
     return url.href;
@@ -132,8 +137,9 @@ Metalsmith(__dirname)
         renderer: renderer
     }))
     .use(contents())
+    .use(tutorials('tutorials')())
     .use(permalinks({
-        pattern: ':filename'
+        duplicates: 'error'
     }))
     .use(i18n()({
         locales: [{
@@ -157,7 +163,6 @@ Metalsmith(__dirname)
         contentPath: 'content/_shadereditor_contents.json',
         partialName: 'shader-editor-navigation'
     }))
-    .use(tutorials('tutorials')())
     .use(layouts({
         pattern: '**/*.html'
     }))

layouts/partials/head.hbs
@@ -8,11 +8,11 @@
 
     <link rel="stylesheet" href="/css/styles.css">
     <link rel="stylesheet" href="/css/playcanvas-theme.css">
-    <link rel="alternate" href="{{{locale-url 'en' path}}}" hreflang="x-default">
-    <link rel="alternate" href="{{{locale-url 'en' path}}}" hreflang="en">
-    <link rel="alternate" href="{{{locale-url 'ja' path}}}" hreflang="ja">
-    <link rel="alternate" href="{{{locale-url 'zh' path}}}" hreflang="zh">
-    <link rel="alternate" href="{{{locale-url 'ru' path}}}" hreflang="ru">
+    <link rel="alternate" href="{{{locale-url 'en' permalink}}}" hreflang="x-default">
+    <link rel="alternate" href="{{{locale-url 'en' permalink}}}" hreflang="en">
+    <link rel="alternate" href="{{{locale-url 'ja' permalink}}}" hreflang="ja">
+    <link rel="alternate" href="{{{locale-url 'zh' permalink}}}" hreflang="zh">
+    <link rel="alternate" href="{{{locale-url 'ru' permalink}}}" hreflang="ru">
 
     <link rel="icon" type="image/png" href="//s3-eu-west-1.amazonaws.com/static.playcanvas.com/images/icons/favicon_docs.png">
 

commit 0d59c96418649f3d5d8aa18aff7a26822e8aa969

fix(build): duplicate nav menu
fix issue introduced in #522

The ordering of the plugins requires localization to run before tutorials.

build.mjs
@@ -137,10 +137,6 @@ Metalsmith(__dirname)
         renderer: renderer
     }))
     .use(contents())
-    .use(tutorials('tutorials')())
-    .use(permalinks({
-        duplicates: 'error'
-    }))
     .use(i18n()({
         locales: [{
             file: 'content/ja/messages.json', locale: 'ja'
@@ -162,6 +158,10 @@ Metalsmith(__dirname)
         template: path.join(__dirname, 'layouts/partials/navigation.hbs'),
         contentPath: 'content/_shadereditor_contents.json',
         partialName: 'shader-editor-navigation'
+    }))
+    .use(tutorials('tutorials')())
+    .use(permalinks({
+        duplicates: 'error'
     }))
     .use(layouts({
         pattern: '**/*.html'