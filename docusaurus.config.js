640cb20f5d209392d50098c387b7da8ea193f1ea
Mark Lundin <mlundin@snapchat.com>
2024-01-15 16:07:50 +0000

    Removed conditional base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -16,7 +16,7 @@ const config = {
   url: 'https://developer.playcanvas.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: process.env.NODE_ENV === 'production' ? '/developer.playcanvas.com/' : '/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
