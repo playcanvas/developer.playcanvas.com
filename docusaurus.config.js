5d63454d233d1253e03dcabadf3719f515d01122
Mark Lundin <mlundin@snapchat.com>
2024-01-19 16:26:23 +0000

    Added DocSearch credentials

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,6 +61,11 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      algolia: {
+        appId: '69QRBNV3NH',
+        apiKey: '1a45aa069915da9c841ad1a9c225e815',
+        indexName: 'developer-playcanvas',
+      },
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
