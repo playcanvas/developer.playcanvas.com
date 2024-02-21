41447b8820fb32c9c464fcd1695722e7029f4123
Will Eastcott <will@playcanvas.com>
2024-02-21 21:15:00 +0000

    Whitespace

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ function HomepageHeader() {
 }
 
 export default function Home() {
-  const { siteConfig } = useDocusaurusContext();
+  const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
       title={translate({
